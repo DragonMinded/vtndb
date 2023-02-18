d3da2b0b4dc9d89c4759186ee4427c22b7251c9e
Jennifer Taylor <dragonminded@dragonminded.com>
2023-02-18 08:15:56 +0000

    Lots of improvements:
     - Handle input, including back/goto/links.
     - Fix back navigation.
     - Display a status bar that can be typed into.
     - Display links and other [] items in bold.
     - Partial cursor handler including ability to move cursor and fetch position.
     - Ability to display non-ascii text using "+".
     - Better response processing that handles fast typing.

--- ndb.py
+++ ndb.py
@@ -1,9 +1,22 @@
 import argparse
 import sys
-from typing import List
+from typing import List, Optional
 
 from wiki import Wiki, Page
 from terminal import Terminal
+
+
+class Action:
+    pass
+
+
+class NavigateAction(Action):
+    def __init__(self, uri: str) -> None:
+        self.uri = uri
+
+
+class BackAction(Action):
+    pass
 
 
 class Navigation:
@@ -16,30 +29,161 @@ class Navigation:
         return self.wiki.getPage(uri)
 
     def canGoBack(self) -> bool:
-        return bool(self.stack)
+        return len(self.stack) > 1
 
     def back(self) -> Page:
         if not self.canGoBack():
             raise Exception("Nothing to go back to!")
 
-        uri = self.stack[-1]
         self.stack = self.stack[:-1]
-        return self.wiki.getPage(uri)
+        return self.wiki.getPage(self.stack[-1])
 
 
 class Rendering:
     def __init__(self, terminal: Terminal) -> None:
         self.terminal = terminal
+        self.input = ""
+        self.page: Optional[Page] = None
 
     def displayPage(self, page: Page) -> None:
+        self.page = page
+
         data = page.data
         if data.startswith("css"):
             data = data[3:]
 
         self.terminal.sendCommand(Terminal.CLEAR_SCREEN)
         self.terminal.sendCommand(Terminal.MOVE_CURSOR_ORIGIN)
+        self.terminal.sendCommand(Terminal.SET_NORMAL)
+        self.terminal.sendCommand(Terminal.SET_BOLD)
         self.terminal.sendText(f"{page.domain.root}:{page.path} -- {page.name}\n\n")
-        self.terminal.sendText(data)
+        self.terminal.sendCommand(Terminal.SET_NORMAL)
+
+        while data:
+            if "[" in data:
+                before, data = data.split("[", 1)
+                if "]" in data:
+                    after, data = data.split("]", 1)
+
+                    self.terminal.sendText(before)
+                    self.terminal.sendCommand(Terminal.SET_BOLD)
+                    self.terminal.sendText("[")
+                    self.terminal.sendText(after)
+                    self.terminal.sendText("]")
+                    self.terminal.sendCommand(Terminal.SET_NORMAL)
+                else:
+                    self.terminal.sendText(before)
+                    self.terminal.sendText("[")
+            else:
+                self.terminal.sendText(data)
+                data = ""
+
+        # Render status bar at the bottom.
+        self.clearInput()
+
+    def clearInput(self) -> None:
+        self.terminal.moveCursor(self.terminal.rows, 1)
+        self.terminal.sendCommand(Terminal.SAVE_CURSOR)
+        self.terminal.sendCommand(Terminal.SET_REVERSE)
+        self.terminal.sendText(" " * self.terminal.columns)
+        self.terminal.sendCommand(Terminal.RESTORE_CURSOR)
+
+        # Clear command.
+        self.input = ""
+
+    def displayError(self, error: str) -> None:
+        # TODO: Display this on the VT-100
+        print(error)
+
+    def processInput(self, inputVal: bytes) -> Optional[Action]:
+        if self.page is None:
+            return None
+        page = self.page
+
+        if inputVal == Terminal.LEFT:
+            row, col = self.terminal.fetchCursor()
+            if col > 1:
+                col -= 1
+                self.terminal.moveCursor(row, col)
+        elif inputVal == Terminal.RIGHT:
+            row, col = self.terminal.fetchCursor()
+            if col < (len(self.input) + 1):
+                col += 1
+                self.terminal.moveCursor(row, col)
+        elif inputVal == Terminal.UP:
+            raise Exception("Not implemented!")
+        elif inputVal == Terminal.DOWN:
+            raise Exception("Not implemented!")
+        elif inputVal == Terminal.BACKSPACE:
+            if self.input:
+                # Just subtract from input.
+                row, col = self.terminal.fetchCursor()
+
+                if col == len(self.input) + 1:
+                    self.input = self.input[:-1]
+
+                    col -= 1
+                    self.terminal.moveCursor(row, col)
+                    self.terminal.sendCommand(Terminal.SET_REVERSE)
+                    self.terminal.sendText(" ")
+                    self.terminal.moveCursor(row, col)
+                else:
+                    raise Exception("Not implemented!")
+        elif inputVal == b"\r":
+            # Ignore this.
+            pass
+        elif inputVal == b"\n":
+            # Execute command.
+            if self.input[0] == "!":
+                # Link navigation.
+                try:
+                    link = int(self.input[1:])
+                    link -= 1
+
+                    if link < 0 or link >= len(page.links):
+                        self.displayError("Unknown link navigation request!")
+
+                    self.clearInput()
+                    if ":" in page.links[link] or not page.links[link].startswith("/"):
+                        # Fully qualified path.
+                        return NavigateAction(page.links[link])
+                    else:
+                        # Assume current domain.
+                        return NavigateAction(f"{page.domain.root}:{page.links[link]}")
+                except ValueError:
+                    self.displayError("Invalid link navigation request!")
+            elif self.input == "back":
+                self.clearInput()
+                return BackAction()
+            elif self.input.startswith("goto"):
+                if " " not in self.input:
+                    self.displayError("No page requested!")
+                else:
+                    _, newpage = self.input.split(" ", 1)
+                    newpage = newpage.strip()
+
+                    if ":" in newpage or not newpage.startswith("/"):
+                        # Fully qualified path.
+                        return NavigateAction(newpage)
+                    else:
+                        # Assume current domain.
+                        return NavigateAction(f"{page.domain.root}:{newpage}")
+            else:
+                self.displayError("Unrecognized command " + self.input)
+        else:
+            # Just add to input.
+            row, col = self.terminal.fetchCursor()
+            char = inputVal.decode("ascii")
+
+            if col == len(self.input) + 1:
+                self.input += char
+                self.terminal.sendCommand(Terminal.SET_REVERSE)
+                self.terminal.sendText(char)
+            else:
+                raise Exception("Not implemented!")
+
+        # Nothing happening here!
+        return None
 
 
 def main(port: str, baudrate: int) -> int:
@@ -54,7 +198,14 @@ def main(port: str, baudrate: int) -> int:
     while True:
         inputVal = terminal.recvInput()
         if inputVal:
-            print(inputVal)
+            action = renderer.processInput(inputVal)
+            if isinstance(action, NavigateAction):
+                page = nav.navigate(action.uri)
+                renderer.displayPage(page)
+            elif isinstance(action, BackAction):
+                if nav.canGoBack():
+                    page = nav.back()
+                    renderer.displayPage(page)
 
     return 0
 
--- terminal.py
+++ terminal.py
@@ -1,6 +1,6 @@
 import serial  # type: ignore
 import time
-from typing import Optional
+from typing import Optional, Tuple
 
 
 class Terminal:
@@ -9,8 +9,20 @@ class Terminal:
     REQUEST_STATUS: bytes = b"[5n"
     STATUS_OKAY: bytes = b"[0n"
 
+    REQUEST_CURSOR: bytes = b"[6n"
+
     MOVE_CURSOR_ORIGIN: bytes = b"[H"
     CLEAR_SCREEN: bytes = b"[2J"
+
+    SET_132_COLUMNS: bytes = b"[?3h"
+    SET_80_COLUMNS: bytes = b"[?3l"
+
+    SET_BOLD: bytes = b"[1m"
+    SET_NORMAL: bytes = b"[m"
+    SET_REVERSE: bytes = b"[7m"
+
+    SAVE_CURSOR: bytes = b"7"
+    RESTORE_CURSOR: bytes = b"8"
 
     UP: bytes = b"[A"
     DOWN: bytes = b"[B"
@@ -30,13 +42,43 @@ class Terminal:
         # Reset terminal.
         self.sendCommand(self.CLEAR_SCREEN)
         self.sendCommand(self.MOVE_CURSOR_ORIGIN)
+        self.sendCommand(self.SET_80_COLUMNS)
+        self.sendCommand(self.SET_NORMAL)
+        self.columns: int = 80
+        self.rows: int = 24
+
+    def set132Columns(self) -> None:
+        self.sendCommand(self.SET_132_COLUMNS)
+        self.columns = 132
+
+    def set80Columns(self) -> None:
+        self.sendCommand(self.SET_80_COLUMNS)
+        self.columns = 80
 
     def sendCommand(self, cmd: bytes) -> None:
         self.serial.write(self.ESCAPE)
         self.serial.write(cmd)
 
+    def moveCursor(self, row: int, col: int) -> None:
+        self.sendCommand(f"[{row};{col}H".encode("ascii"))
+
+    def fetchCursor(self) -> Tuple[int, int]:
+        self.sendCommand(self.REQUEST_CURSOR)
+        resp = self.recvResponse()
+        if resp[:1] != b"[" or resp[-1:] != b"R":
+            raise Exception("Invalid response for cursor fetch!")
+        respstr = resp[1:-1].decode("ascii")
+        row, col = respstr.split(";", 1)
+        return int(row), int(col)
+
     def sendText(self, text: str) -> None:
-        self.serial.write(text.encode("ascii"))
+        def fb(data: str) -> bytes:
+            try:
+                return data.encode("ascii")
+            except UnicodeEncodeError:
+                return b"+"
+
+        self.serial.write(b"".join(fb(s) for s in text))
 
     def recvResponse(self, timeout: Optional[float] = None) -> bytes:
         gotResponse: bool = False
@@ -53,7 +95,32 @@ class Terminal:
                         accum = accum[1:]
 
                     if accum and accum[0:1] == self.ESCAPE:
-                        return accum[1:]
+                        # We could have some regular input after this. So parse the command a little.
+                        accum = accum[1:]
+
+                        for offs in range(len(accum)):
+                            val = accum[offs : (offs + 1)]
+                            if val not in {
+                                b"0",
+                                b"1",
+                                b"2",
+                                b"3",
+                                b"4",
+                                b"5",
+                                b"6",
+                                b"7",
+                                b"8",
+                                b"9",
+                                b";",
+                                b"?",
+                                b"[",
+                            }:
+                                # This is the last character.
+                                if accum[offs + 1 :]:
+                                    self.pending += accum[offs + 1 :]
+                                return accum[: (offs + 1)]
+
+                        raise Exception("Should have found end of command marker!")
                     else:
                         accum = b""
                         if timeout:
